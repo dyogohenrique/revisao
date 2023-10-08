387dcef7a8908c0d979468eb0213a95619d3527a
Dyogo Henrique <105476232+dyogohenrique@users.noreply.github.com>
2023-10-07 21:54:38 -0400

    mais

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let nome = document.querySelector("#nome").value;
+let dataNascimento = document.querySelector("#dataNascimento").value;
+let cpf = document.querySelector("#cpf").value;
+
+let cep = document.querySelector("#cep").value;
+let bairro = document.querySelector("#bairro").value;
+let rua = document.querySelector("#rua").value;
+let numero = document.querySelector("#numero").value;
+
+let sexo = document.querySelector("input[name='sexo']:checked");
+
+let metodoPagamento = document.querySelector("input[name='metodoPagamento]:checked'");
+
+let numCartao = '';
+let validade = '';
+let codigoSeguranca = '';
+
+if (metodoPagamento === "Cartão de Crédito") {
+    numCartao = document.querySelector("#numeroCartao");
+    validade = document.querySelector("#validade");
+    codigoSeguranca = document.querySelector("#codigoSeguranca")
+}
+
+let campoCartao = document.querySelector("#cartaoCredito-continer")
+
+function mostrarCampoCartao(exibir) {
+    if (exibir) {
+        campoCartao.style.display = "block"
+    } else {
+        campoCartao.style.display = "none"
+    }
+}
